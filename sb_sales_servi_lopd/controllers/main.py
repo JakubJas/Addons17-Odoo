2caf7d355a3e78fba9afa7c62c7f2011e0937b6e
JakubJ2001 <jjasinski1234@gmail.com>
2026-05-05 14:59:44 +0200

    Cambio de estado y firma de LOPD

--- sb_sales_servi_lopd/controllers/main.py
+++ sb_sales_servi_lopd/controllers/main.py
@@ -1,11 +1,28 @@
-from odoo import http
+from odoo import http, fields
 from odoo.http import request
-from odoo import fields
+
 
 class ServilopdController(http.Controller):
 
     @http.route('/lopd/form/<string:token>', type='http', auth='public', website=True)
     def lopd_form(self, token=None, **kwargs):
+        lopd_request = request.env['servilopd.request'].sudo().search([
+            ('token', '=', token)
+        ], limit=1)
+
+        if not lopd_request:
+            return request.render('sb_sales_servi_lopd.token_invalid')
+
+        if lopd_request.token_expiration and lopd_request.token_expiration < fields.Datetime.now():
+            return request.render('sb_sales_servi_lopd.token_expired')
+
+        return request.render('sb_sales_servi_lopd.lopd_form_template', {
+            'request_record': lopd_request
+        })
+
+    @http.route('/lopd/form/submit', type='http', auth='public', website=True, csrf=False)
+    def lopd_form_submit(self, **post):
+        token = post.get('token')
 
         lopd_request = request.env['servilopd.request'].sudo().search([
             ('token', '=', token)
@@ -14,10 +31,13 @@ class ServilopdController(http.Controller):
         if not lopd_request:
             return request.render('sb_sales_servi_lopd.token_invalid')
 
-        # comprobar expiración
-        if lopd_request.token_expiration and lopd_request.token_expiration < fields.Datetime.now():
-            return request.render('sb_sales_servi_lopd.token_expired')
-
-        return request.render('sb_sales_servi_lopd.lopd_form_template', {
-            'request_record': lopd_request
+        lopd_request.write({
+            'state': 'answered',
+            'response_date': fields.Datetime.now(),
         })
+
+        lopd_request.partner_id.write({
+            'lopd_state': 'signed',
+        })
+
+        return request.render('sb_sales_servi_lopd.lopd_thanks')
